556b62f5b8d93d00142b1b4b017f281a4de99c1e
Sergio Mercado-Nunez <sm9276@banjo.rit.edu>
2024-10-26 23:45:31 -0400

    in progress ...

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+   <head> 
+	<title> Sergio Mercado Nuñez </title>
+	<meta charset="UTF-8">
+
+   </head>
+
+   <body>
+	<!-- Navigation  -->	
+	<h1> Navigation </h1>
+
+
+   </body>
+</html>
