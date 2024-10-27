9038b3b7f66e1cff3229939b48e938bc97321661
Sergio Mercado-Nunez <sm9276@banjo.rit.edu>
2024-10-27 00:25:31 -0400

    about me

--- index.php
+++ index.php
@@ -7,9 +7,36 @@
    </head>
 
    <body>
+	<div>
 	<!-- Navigation  -->	
-	<h1> Navigation </h1>
-
+	   <h1> Navigation </h1>
+		<a href="#">Home</a>
+		<a href="#">Projects</a>
+		<a href="#">Expierence</a>
+		<a href="#">BLOG</a>
+	</div>
+	
+	<!-- Header  -->	
+	<div>
+		<h1>Welcome to my Personal Portfolio Website</h1>
+		<p> I am third year Computer Engineering student at the Rochester Institute of Technology</p>
+	</div>	
+	<!-- AboutMe  -->	
+	<div>
+		<h1>About me</h1>
+		<p> Hello! This is my first website using PHP, HTML, and CSS, and it's now live on the internet!
+		I am a computer engineering student at the Rochester Institute of Technology. 
+		I love learning about computers and how they work!
+		My most recent obsession has been VHDL,  and I am currently working on a 
+		Gameboy Hardware Emulator using it.	
+		I am also working as a Research Assistant at the Golisano Institute of Technology.
+		The project that I created for the research is a tool called PLEXOS to Bokeh Pivot(P2BP) 
+		written in Python. I love the feeling of performing and dancing mexican folk since
+		I get to spread my beautiful culture .
+		I also love my pet dog Oreo
+	</div>	
 
    </body>
+
+
 </html>
