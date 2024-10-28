0bd515d483a53a3e74801364230bc9c201e98019
SM9276 <SM9276@g.rit.edu>
2024-10-27 23:54:40 -0400

    php

---
+++ about.php
@@ -0,0 +1,91 @@
+<!-- about.php -->
+<section class="about-section">
+    <div class="about">
+        <div class="about-img-container">
+            <img src="img/oreo.png" class="about-img" alt=""> 
+            <button class="download-cv-btn">Download CV</button>
+        </div>
+        <p class="about-info">
+            Hello! My Name is Sergio Mercado Nuñez. This is my first website using PHP, HTML, and CSS, 
+            and it's now live on the internet! I am a computer engineering student at the Rochester Institute of Technology. 
+            I love learning about computers and how they work!
+            My most recent obsession has been VHDL, and I am currently working on a Gameboy Hardware Emulator using it.	
+            I am also currently working as a Research Assistant at the Golisano Institute of Technology.
+            The project that I created for the research is a tool called PLEXOS to Bokeh Pivot(P2BP) 
+            written in Python. I love the feeling of performing and dancing mexican folk since
+            I get to spread my beautiful culture .
+            I also love my pet dog Oreo 
+        </p>
+    </div>    
+    <!-- Skills Section -->    
+    <div class="skill-section">
+        <h1 class="heading">Skills</h1> 
+        <div class="skill-container">
+            <div class="skill-card">
+                <img src="img/VHDL.png" class="skill-img" alt=""> 
+                <h1 class="skill-name">VHDL</h1>
+                <p class="skill-info">VHDL RULES!</p> 
+            </div>
+        </div>
+    </div>
+   <!-- timeline.php -->
+    <div class="timeline">
+    <h1 class="heading">My Computer Engineering Journey</h1>
+     <!-- First Computer  -->	
+        <div class="card">
+            <div class ="card-body">
+                <h1 class="card-title"> 2012 </h1>     
+                <p class="card-detail"> The first computer I interacted with was my father's Windows XP machine. 
+                Since I was always such a curious child, I ended up dismantling it.
+                I really wanted to know how this box displayed an image. My father was really mad that night! </p>
+            </div>
+        </div>
+        
+        <!-- Custom OS  -->	
+        <div class="card">
+            <div class ="card-body">
+                <h1 class="card-title"> 2015 </h1>     
+                <p class="card-detail"> I got into installing custom Andriod OSes, I was rocking Linage OS running Andriod 7.1.2 on a Galaxy J7.
+                I ended up helping the community around me, I would help the farm workers unlocking their SIM cards
+                and improving their phones. </p>
+            </div>
+        </div>
+
+        <!-- Computer build -->	
+        <div class="card">
+            <div class ="card-body">
+                <h1 class="card-title"> 2016 </h1>     
+                <p class="card-detail"> A series of slow cheap laptops led me to want to build my own desktop computer. 
+                    I still use this machine to this day, i have upgraded a few parts but the i7-7700 is still going strong. </p>
+            </div>
+        </div>
+
+         <!-- First CS Class  -->	
+        <div class="card">
+            <div class ="card-body">
+                <h1 class="card-title"> 2018-2019 </h1>     
+                <p class="card-detail"> I took my first computer science class in my Freshman Year of High school, 
+                We learned the fundementals of boolean logic and did assignments in html, css </p>
+            </div>
+        </div>
+
+         <!-- AP Computer Science Class -->	
+        <div class="card">
+            <div class ="card-body">
+                <h1 class="card-title"> 2019-2020 </h1>     
+                <p class="card-detail"> Softmore year of Highschool I took AP Computer Science, 
+                I learned about Object Oriented Programming and built assignements in Java </p>
+            </div>
+        </div>
+
+        <!-- Rochester Institute of Technology-->	
+        <div class="card">
+            <div class ="card-body">
+                <h1 class="card-title"> 2022 </h1>     
+                <p class="card-detail"> I started attending the Rochester Institute of Technology for a Computer Engineering Degree </p>
+            </div>
+        </div>
+</div>
+ 
+</section>
+
---
+++ projects.php
@@ -0,0 +1,34 @@
+<!-- projects.php -->
+<section class="project-section">
+    <h1 class="project-heading">Check out my Recent Projects:</h1> 
+    <div class="project-container">
+
+        <div class="project-card">
+            <img src="img/project-1.png" class="project-img" alt="">
+            <div class="project-content">
+                <h1 class="project-title">PLEXOS to Bokeh Pivot (P2BP)</h1>
+                <p class="project-info">A Python tool that converts PLEXOS XML solution files to CSV format 
+                    and visualizes data using Bokeh Pivot, designed for scenario comparison.</p>
+                <div class="project-btn-grp">
+                    <button class="project-btn github">github repo</button>
+                    <button class="project-btn readmore">read more</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-card">
+                <img src="img/project-2.png" class="project-img" alt="">
+                <div class="project-content">
+                    <h1 class="project-title">GroSal</h1>
+                    <p class="project-info">
+                    A React Native app with Expo, and JavaScript that allows weekly budget tracking,
+                    expense recording, and visualization of spending habits. </p>
+                    <div class="project-btn-grp">
+                        <button class="project-btn github">github repo</button>
+                        <button class="project-btn readmore">read more</button>
+                    </div>
+                </div>
+            </div>
+    </div>
+</section>
+
